1dbe60306c486879cc7c9c5ee04d590ce0e7c18e
shwet oberoi <shwetoberoi07@gmail.com>
2020-04-29 13:11:42 +0530

    Updates

---
+++ precache-manifest.6a3087b9d1673afdf356a381926d4af9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c35dce59ac829877e5ed3bc2acea6d45",
+    "url": "/monsters-rolorex/index.html"
+  },
+  {
+    "revision": "46f0f882c4b3252dd0b5",
+    "url": "/monsters-rolorex/static/css/main.002b91e6.chunk.css"
+  },
+  {
+    "revision": "edb20354a1e372d705ca",
+    "url": "/monsters-rolorex/static/js/2.0b091c2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolorex/static/js/2.0b091c2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46f0f882c4b3252dd0b5",
+    "url": "/monsters-rolorex/static/js/main.7a854d24.chunk.js"
+  },
+  {
+    "revision": "0de5d6956f0735b81576",
+    "url": "/monsters-rolorex/static/js/runtime-main.8f04b701.js"
+  }
+]);
